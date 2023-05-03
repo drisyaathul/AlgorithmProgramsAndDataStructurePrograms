9d9060dfabae10ef8ab263a3a70cb0033fcb3c37
drisyaathul <drisyak809@gmail.com>
2023-05-03 16:10:57 +0800

    Q4: Bubble Sort

--- src/com/bridgelabz/AlgorithmPrograms/BubbleSort.java
+++ src/com/bridgelabz/AlgorithmPrograms/BubbleSort.java
@@ -3,8 +3,24 @@ package com.bridgelabz.AlgorithmPrograms;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BubbleSort {
-
+public class BubbleSort <T>{
+    public static <T extends Comparable<T>> void sorting(T[] array) {
+        /*
+         Sorting the Integers by using Bubble sort
+         */
+        int n = array.length;
+        for (int i=0; i < n-1; i++) {
+            for (int j=0; j < n-1; j++) {
+                if (array[j].compareTo(array [j+1]) > 0) {   //Comparing between two elements
+                    T temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("After Sorting :-");
+        System.out.println(Arrays.toString(array));
+    }
     public static void main(String[] args) {
         /*
          * Read the list of Integers in an Array using scanner
@@ -15,7 +31,7 @@ public class BubbleSort {
         /*
          * enter the elements in the array using for loop
          */
-        int [] array = new int[size];
+        Integer [] array = new Integer[size];
         for (int i=0; i<size; i++) {
             System.out.println("Enter the element : ");
             array [i] = scanner.nextInt();
@@ -23,23 +39,13 @@ public class BubbleSort {
         System.out.println("Print the elements in an array (Before Sorting):- ");
         //to String method is to return the string representation of the object.
         System.out.println(Arrays.toString(array));
-        /*
-         Sorting the Integers by using Bubble sort
-         */
-        int n = array.length;
-        for (int i=0; i < n-1; i++) {
-            for (int j=0; j < n-1; j++) {
-                if (array[j] > array [j+1]) {   //Comparing between two elements
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                }
-            }
-        }
+        sorting(array);
+
         /*
         Printing the Sorting list
          */
-        System.out.println("After Sorting :-");
-        System.out.println(Arrays.toString(array));
+
     }
+
+
 }
