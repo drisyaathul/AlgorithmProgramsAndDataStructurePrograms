ed09c88387dc83139ea69b537f4080f001966b59
drisyaathul <drisyak809@gmail.com>
2023-05-03 12:46:40 +0800

    Q3: Insertion Sort

---
+++ src/com/bridgelabz/AlgorithmPrograms/InsertionSort.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.AlgorithmPrograms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort{
+
+    public static void sorting(String[] array) {
+        //sort string array using Arrays.sort method
+        System.out.println("Sorted order is:- ");
+        for(int i=0;i<array.length;i++)
+        {
+            Arrays.sort(array);
+            System.out.println(array[i]);
+        }
+    }
+    public static void main(String[] args) {
+        /*
+            Reads in strings and prints them in sorted order using insertion sort.
+         */
+        {
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Enter the number of words to be printed: ");
+            int n=scanner.nextInt();
+
+            //create string array
+            String array[]=new String[n];
+            System.out.println();
+            System.out.println("Enter the words to be printed: ");
+            for(int i=0;i<array.length;i++)
+            {
+                array[i]=scanner.next();
+            }
+            System.out.println();
+            sorting(array);
+        }
+    }
+}
+
+
+
+
+
+
