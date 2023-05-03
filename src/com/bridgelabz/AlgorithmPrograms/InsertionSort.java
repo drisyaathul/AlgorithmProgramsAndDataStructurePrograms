56a886f3b4f479a59f1469357e13f49afc59fe16
drisyaathul <drisyak809@gmail.com>
2023-05-03 12:54:48 +0800

    Q3: Insertion Sort

--- src/com/bridgelabz/AlgorithmPrograms/InsertionSort.java
+++ src/com/bridgelabz/AlgorithmPrograms/InsertionSort.java
@@ -8,7 +8,8 @@ public class InsertionSort{
     public static void sorting(String[] array) {
         //sort string array using Arrays.sort method
         System.out.println("Sorted order is:- ");
-        for(int i=0;i<array.length;i++)
+
+        for(int i=0; i<array.length; i++)
         {
             Arrays.sort(array);
             System.out.println(array[i]);
@@ -19,17 +20,17 @@ public class InsertionSort{
             Reads in strings and prints them in sorted order using insertion sort.
          */
         {
-            Scanner scanner=new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Enter the number of words to be printed: ");
-            int n=scanner.nextInt();
+            int size = scanner.nextInt();
 
             //create string array
-            String array[]=new String[n];
+            String array[] = new String[size];
             System.out.println();
             System.out.println("Enter the words to be printed: ");
-            for(int i=0;i<array.length;i++)
+            for(int i=0; i<array.length; i++)
             {
-                array[i]=scanner.next();
+                array[i] = scanner.next();
             }
             System.out.println();
             sorting(array);
